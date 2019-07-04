65b2a91e276e5de610d68f82b0986bb1f4bb7118
Leonardo Luxburg <leo.von.luxburg@gmail.com>
2019-07-04 13:55:56 -0500

    upload time

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get '/news', to: 'pages#news'
+  get '/projects', to: 'pages#projects'
+  get '/team', to: 'pages#team'
+  get '/contact', to: 'pages#contact'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
