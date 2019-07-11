7b4e8e6eb59b12fc3c37369155cf4488fba41c5c
Leonardo Luxburg <leo.von.luxburg@gmail.com>
2019-07-11 15:22:26 -0500

    Heroku Error

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,22 +1,3 @@
 //= require rails-ujs
 //= require_tree .
 //= require owl.carousel
-
-document.addEventListener("turbolinks:load", function() {
-  $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
-})
-});
